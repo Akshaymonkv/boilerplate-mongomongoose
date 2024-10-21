ea1f6f677773e23bce9a11c0ec887e3b0bfdf53b
Akshaymon K V <kvakshaymon@gmail.com>
2024-10-21 22:10:26 +0530

    Learing about mongo functions

--- myApp.js
+++ myApp.js
@@ -2,20 +2,51 @@ require('dotenv').config();
 let Mongoose = require('mongoose')
 
 
-let Person;
+
+let personSchema = new Mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: Number,
+  favoriteFoods: [String]
+})
+
+let Person = Mongoose.model('Person',personSchema)
 
 Mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 //MONGO_URI
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+
+//creating an object instance fot the Person object model
+  var newP = new Person({
+    name: "Akshay",
+    age: 25,
+    favoriteFoods: ["dosa","Idli"]
+  })
+    
+//save method is used to save an object to the database
+  newP.save(function(err,data){
+    if(err)return console.log(err)
+      done(null, data);
+  })
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+//if we are going to create multiple entries into the database we can feed them as an array of object and we can use Model.create()
+  Person.create(arrayOfPeople,(err,data)=>{
+    if(err) return done(err)
+      done(null,data);
+  })
 };
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+//To find a single value in it, in this case we can see personName is given as an object and not string because our document has objects with "name" in it to match this to
+  Person.find({name: personName},(err,data)=>{
+    if(err) return done(err)
+      done(null,data);
+  })
+ // done(null /*, data*/);
 };
 
 const findOneByFood = (food, done) => {
